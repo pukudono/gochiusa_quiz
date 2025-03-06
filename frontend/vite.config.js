0a1e61ad30eb57c03b7cf464bbe691cfdf74dd4f
pukudono <koukou7990@gmail.com>
2025-03-07 02:29:48 +0900

    Update vite.config.js and deploy frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/gochiusa_quiz/", // GitHub Pages用に修正
   plugins: [react()],
+  base: '/gochiusa_quiz/', // ここをリポジトリ名に変更
 });
